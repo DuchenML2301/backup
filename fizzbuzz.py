68ab65d76a0446372cfc323632eb506835c60e66
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-30 13:11:48 -0700

    YES

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,11 @@
 for num in range(1, 100):
 	if num % 3 == 0:
-		print("Fizz")
-    if num % 5 == 0:
-    	print("Buzz")
-    if num
+		print("Fizz", end="")
+		print(num)
+	if num % 5 == 0:
+		print("Buzz", end="")
+		print(num)
+	if (num % 5 == 0 or num % 3 == 0):
+		print("fizz", end="")
+		print("buzz", end="")
+		print(num)
