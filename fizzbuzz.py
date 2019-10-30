32f374c1bf5908f72abb1966e1368785644eda8b
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-30 13:15:44 -0700

    I DID IT

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-for num in range(1, 100):
+for num in range(1, 101):
 	if num % 3 == 0:
 		print("Fizz", end="")
 		print(num)
@@ -8,4 +8,6 @@ for num in range(1, 100):
 	if (num % 5 == 0 or num % 3 == 0):
 		print("fizz", end="")
 		print("buzz", end="")
+		print(num)
+	else:
 		print(num)
