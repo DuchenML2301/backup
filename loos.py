9afd38bf89634c292d753a78f6a7cc8d86bc8d69
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-30 22:28:02 -0700

    Update loos.py

--- loos.py
+++ loos.py
@@ -38,3 +38,13 @@ while True:
 		os.system("cls")
 		print(frame)
 		time.sleep(.3)
+num = 0 
+While num <=100:
+Print(num)
+num = num + 1
+if num % 3 + num % 5 == 0:
+Print("FizzBuzz", end="")
+elif num % 3 == 0:
+Print("Fizz", end="")
+elif num % 5 == 0:
+Print("Buzz", end"")
