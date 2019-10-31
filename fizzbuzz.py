7a2d2160274259d89e95261b392f0e8122abb9df
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-10-30 20:39:07 -0700

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,15 @@
 for num in range(1, 101):
+
+        if (num % 5 == 0 or num % 3 == 0):
+		print("fizz", end="")
+		print("buzz", end="")
+		print(num)
+
 	if num % 3 == 0:
 		print("Fizz", end="")
 		print(num)
 	if num % 5 == 0:
 		print("Buzz", end="")
-		print(num)
-	if (num % 5 == 0 or num % 3 == 0):
-		print("fizz", end="")
-		print("buzz", end="")
 		print(num)
 	else:
 		print(num)
