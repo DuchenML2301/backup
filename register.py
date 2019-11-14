e662a878b592c3c71868e4938285b5711fe57d9b
DuchenML2301 <56047105+DuchenML2301@users.noreply.github.com>
2019-11-13 17:17:59 -0700

    tyftu

--- register.py
+++ register.py
@@ -1,58 +1,158 @@
+import time
+import os
+import randomimport time
+import os
+import random
 
-pennys = 0
-nickels = 0
-dimes = 0
-quarters = 0
-half = 0
-ones = 0
+item = input("Enter the cost of the item: ")
 
-price = int(input("What is the price of the item?: "))
-amount = int(input("How much was given?: "))
+print("The item is $ " + str(item))
 
-change = amount - price
-print("Change:", (change))
+dollars = input("Enter the number of dollars you will pay:")
 
-pennys += nickels * 5
-nickels += dimes * 2
-dimes += quarters * 2.5
-quarters += half * 2
-half += ones * 2
+icents = 0
+icents = float(item) * 100
+pcents = 0
+total = 0
+finalprintd = 0
+finalprintc = 0
+finalprintdim = 0
+finalprintn = 0
+finalprintp = 0
+finalprinth = 0
 
-pennys = 0
-nickels = 0
-dimes = 0
-quarters = 0
-half = 0
-ones = 0
-while ones >= change:
-	pennys = 0
-	nickels = 0
-	dimes = 0
-	quarters = 0
-	half = 0
-	ones = 0
-	
-	ones = int(half / 2)
-	half = half % 2
-	half = int(quarters / 2)
-	quarters = quarters % 2
-	quarters = int(dimes / 2.5)
-	dimes = dimes % 2.5
-	dimes = int(nickels / 2)
-	nickels = nickels % 2
-	nickels = int(pennys / 5)
-	pennys = pennys % 5
+print("You payed $" + str(dollars))
 
-	print(str(ones) + "ones")
-	print(str(half) + "half")
-	print(str(quarters) + "quarters")
-	print(str(dimes) + "dimes") 
-	print(str(nickels) + "nickels")
-	print(str(pennys) + "pennys")
+pcents += int(dollars) * int(100)
 
-	pennys += nickels * 5
-	nickels += dimes * 2
-	dimes += quarters * 2.5
-	quarters += half * 2
-	half += ones * 2
-print(ones)
+#rint(icents)
+
+#rint(pcents)  
+
+finalc = pcents - icents
+
+#rint(finalc)
+
+total = finalc / 100
+
+#rint(finald)
+
+print("You got $" + str(total) + " back.")
+print("")
+
+while finalc > 100:
+	finalprintd += 1
+	finalc -= 100
+print("You got " + str(finalprintd) + " dollar bills.")
+print("")
+
+while finalc > 50:
+	finalprinth +=1
+	finalc -= 50
+print("You got " + str(finalprinth) + " half dollars.")
+print("")
+
+
+while finalc > 25:
+	finalprintc += 1
+	finalc -= 25
+print("You got " + str(finalprintc) + " quarters.")
+print("")
+
+
+if finalc < 25:
+	while finalc > 10:
+		finalprintdim += 1
+		finalc -=10
+	print("You got " + str(finalprintdim) + " dimes.")
+	print("")
+
+	while finalc > 5:
+		finalprintn += 1
+		finalc -=5
+	print("You got " + str(finalprintn) + " nickels.")
+	print("")
+
+	while finalc > 0:
+		finalprintp += 1
+		finalc -=1
+	print("You got " + str(finalprintp) + " pennies.")
+	print("")
+
+
+
+
+item = input("Enter the cost of the item: ")
+
+print("The item is $ " + str(item))
+
+dollars = input("Enter the number of dollars you will pay:")
+
+icents = 0
+icents = float(item) * 100
+pcents = 0
+total = 0
+finalprintd = 0
+finalprintc = 0
+finalprintdim = 0
+finalprintn = 0
+finalprintp = 0
+finalprinth = 0
+
+print("You payed $" + str(dollars))
+
+pcents += int(dollars) * int(100)
+
+#rint(icents)
+
+#rint(pcents)  
+
+finalc = pcents - icents
+
+#rint(finalc)
+
+total = finalc / 100
+
+#rint(finald)
+
+print("You got $" + str(total) + " back.")
+print("")
+
+while finalc > 100:
+	finalprintd += 1
+	finalc -= 100
+print("You got " + str(finalprintd) + " dollar bills.")
+print("")
+
+while finalc > 50:
+	finalprinth +=1
+	finalc -= 50
+print("You got " + str(finalprinth) + " half dollars.")
+print("")
+
+
+while finalc > 25:
+	finalprintc += 1
+	finalc -= 25
+print("You got " + str(finalprintc) + " quarters.")
+print("")
+
+
+if finalc < 25:
+	while finalc > 10:
+		finalprintdim += 1
+		finalc -=10
+	print("You got " + str(finalprintdim) + " dimes.")
+	print("")
+
+	while finalc > 5:
+		finalprintn += 1
+		finalc -=5
+	print("You got " + str(finalprintn) + " nickels.")
+	print("")
+
+	while finalc > 0:
+		finalprintp += 1
+		finalc -=1
+	print("You got " + str(finalprintp) + " pennies.")
+	print("")
